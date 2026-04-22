be70732dc0ace08b79ae0369409b79c3098fc0a7
hyunbeomlee1227-dev <hyunbeom.lee1227@gmail.com>
2026-04-22 16:49:58 +0900

    [faet] Reflections 기능을 이용하여 특정 폴더 아래에 있는 컴포넌트를 모두 불러와 생성할 수 있도록 구현 -> 현재 인자가 없는 Repository 구현(t4)

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,14 +1,50 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext(String basePackage) {
+import com.ll.framework.ioc.annotations.Component;
+import org.reflections.Reflections;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ApplicationContext {
+    private Reflections reflections;
+    private final Map<String, Object> singletonObjects = new HashMap<>();
+    private final Map<String, Class<?>> beanDefinitionMap = new HashMap<>();
+
+    public ApplicationContext(String basePackage) {
+        reflections = new Reflections(basePackage);
     }
 
     public void init() {
+        Set<Class<?>> components = reflections.getTypesAnnotatedWith(Component.class);
+
+        for (Class<?> clazz : components) {
+            String beanName = clazz.getSimpleName().substring(0, 1).toLowerCase()
+                    + clazz.getSimpleName().substring(1);
+
+            beanDefinitionMap.put(beanName, clazz);
+        }
     }
 
     public <T> T genBean(String beanName) {
-        return null;
+        Object singleton = singletonObjects.get(beanName);
+        if (singleton != null) {
+            return (T) singleton;
+        }
+
+        Class<?> someClass = beanDefinitionMap.get(beanName);
+        if (someClass == null) {
+            throw new RuntimeException("Bean not found: " + beanName);
+        }
+
+        try {
+            Object instance = someClass.getDeclaredConstructor().newInstance();
+            singletonObjects.put(beanName, instance);
+
+            return (T) instance;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
