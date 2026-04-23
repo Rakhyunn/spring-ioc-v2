c2c5b9e6bb33d892668677e82f062262bd9b2c95
Rakhyunn <krh0323@pukyong.ac.kr>
2026-04-23 16:44:36 +0900

    [feat] bean 등록을 위한 Component 어노테이션이 있는 클래스 탐색 구현

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,6 +1,10 @@
 package com.ll.framework.ioc;
 
+import com.ll.framework.ioc.annotations.Component;
+import com.ll.standard.util.Ut;
+
 import java.io.File;
+import java.lang.annotation.Annotation;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,17 +19,19 @@ public class ApplicationContext {
 
     public void init() {
         try {
+            // 패키지 경로를 파일 시스템 경로로 변환
             String path = basePackage.replace(".", "/");
             ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
             URL resource = classLoader.getResource(path);
             File directory = new File(resource.toURI());
 
-            scanDirectory(directory, basePackage);
+            scanDirectory(directory, basePackage);  // 탐색 시작
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    // 재귀로 패키지 하위의 클래스 파일까지 탐색 메서드
     private void scanDirectory(File directory, String packageName) {
         for (File file : directory.listFiles()) {
             if (file.isDirectory()) {
@@ -33,9 +39,30 @@ public class ApplicationContext {
             } else if (file.getName().endsWith(".class")) {
                 String className = file.getName().replace(".class", "");
                 String fullClassName = packageName + "." + className;
-                System.out.println("스캔된 클래스: " + fullClassName);
+                try {
+                    Class<?> clazz = Class.forName(fullClassName);
+                    if (isComponent(clazz)) {
+                        className = Ut.str.lcfirst(className);  // 앞글자를 소문자로 변환
+                        System.out.println("Bean 등록: " + className);
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         }
+    }
+
+    // Component 어노테이션 여부 반환 메서드
+    private boolean isComponent(Class<?> clazz) {
+        if(clazz.isAnnotationPresent(Component.class)) return true;
+
+        for(Annotation annotation : clazz.getAnnotations()) {
+            if(annotation.annotationType().isAnnotationPresent(Component.class)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public <T> T genBean(String beanName) {
